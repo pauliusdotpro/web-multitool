636bb759702c5049addb5e5d2bf7b3385e2a78d8
Paul <13413990+pauliusdotpro@users.noreply.github.com>
2024-09-27 21:22:06 +0300

    Add umami tracking code

--- apps/web/src/pages/layout.tsx
+++ apps/web/src/pages/layout.tsx
@@ -4,6 +4,8 @@ import Sidebar from "@/components/sidebar";
 export default function Layout({ children } : any) {
     return (
         <>
+            <script defer src="https://umami.paulius.pro/script.js"
+                    data-website-id="d0f46837-fb44-4c5d-8a9d-7ad980a027a4"></script>
             <Sidebar>
                 <main>{children}</main>
 

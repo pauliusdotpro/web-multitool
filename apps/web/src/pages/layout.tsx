cb3b9258e03cc29a835a770ed6c27d1c2d19828b
Paul <13413990+pauliusdotpro@users.noreply.github.com>
2024-10-11 13:24:20 +0300

    Add google analytics

--- apps/web/src/pages/layout.tsx
+++ apps/web/src/pages/layout.tsx
@@ -1,11 +1,13 @@
 import FollowMe from "@/components/follow-me";
 import Sidebar from "@/components/sidebar";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function Layout({ children } : any) {
     return (
         <>
             <script defer src="https://umami.paulius.pro/script.js"
                     data-website-id="d0f46837-fb44-4c5d-8a9d-7ad980a027a4"></script>
+            <GoogleAnalytics gaId="G-3247P7MLM6" />
             <Sidebar>
                 <main>{children}</main>
 
